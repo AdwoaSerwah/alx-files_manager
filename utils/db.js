0ee25b0585c0392e43668b45be3e244e7fe54dae
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-25 01:24:20 +0000

    Add task 1

--- utils/db.js
+++ utils/db.js
@@ -6,73 +6,47 @@ const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 /**
- * Class for performing operations with MongoDB service
+ * Class for performing operations with Mongo service
  */
 class DBClient {
   constructor() {
-    this.db = null;
-    this.usersCollection = null;
-    this.filesCollection = null;
-    this.isConnecting = false;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
   }
 
   /**
-   * Establishes a connection to MongoDB and initializes collections
-   * @returns {Promise<void>}
-   */
-  async connect() {
-    if (this.isConnecting) {
-      return;
-    }
-    this.isConnecting = true;
-    try {
-      const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-      this.db = client.db(DB_DATABASE);
-      this.usersCollection = this.db.collection('users');
-      this.filesCollection = this.db.collection('files');
-      console.log('Connected successfully to MongoDB');
-    } catch (err) {
-      console.error('MongoDB connection error:', err.message);
-      this.db = false;
-    }
-  }
-
-  /**
-   * Checks if the connection to MongoDB is alive
-   * @return {boolean} true if connected, false otherwise
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
    */
   isAlive() {
     return Boolean(this.db);
   }
 
   /**
-   * Returns the number of documents in the users collection
-   * @return {Promise<number>} Number of users
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
    */
   async nbUsers() {
-    if (!this.db) {
-      await this.connect(); // Ensure we connect before performing the operation
-    }
-    if (this.db) {
-      const numberOfUsers = await this.usersCollection.countDocuments();
-      return numberOfUsers;
-    }
-    throw new Error('Not connected to MongoDB');
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
   /**
-   * Returns the number of documents in the files collection
-   * @return {Promise<number>} Number of files
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
    */
   async nbFiles() {
-    if (!this.db) {
-      await this.connect(); // Ensure we connect before performing the operation
-    }
-    if (this.db) {
-      const numberOfFiles = await this.filesCollection.countDocuments();
-      return numberOfFiles;
-    }
-    throw new Error('Not connected to MongoDB');
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
 
