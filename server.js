d9b00954bc53753e7f5a1b09aedd95ea77e41f9b
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-25 00:49:55 +0000

    Add task 0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import controllerRouting from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
